23b3833ef5c77c326870ce44dc0a12a5cb18a279
vinicius santos <viniciussantos@jera.com.br>
2021-05-15 10:41:03 -0400

    adding saved instance state

--- Navigation/app/src/main/java/com/drawiin/navigation/LoginActivity.java
+++ Navigation/app/src/main/java/com/drawiin/navigation/LoginActivity.java
@@ -1,13 +1,13 @@
 package com.drawiin.navigation;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-import androidx.databinding.ObservableInt;
-
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.drawiin.navigation.databinding.ActivityLoginBinding;
 import com.google.android.material.textfield.TextInputLayout;
@@ -16,24 +16,37 @@ import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
+    private TextInputLayout[] inputs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
         setupUi();
+        if (savedInstanceState != null) recoveryState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        for (final TextInputLayout item : inputs) {
+            final String key = String.valueOf(item.getId());
+            final String text = String.valueOf(Objects.requireNonNull(item.getEditText()).getText()).trim();
+            Log.i(TAG, "Saving State " + text);
+            outState.putString(key, text);
+        }
+        super.onSaveInstanceState(outState);
     }
 
     private void setupUi() {
         binding.toolbar.setNavigationOnClickListener((view) -> onBackPressed());
-        final TextInputLayout[] inputs = new TextInputLayout[]{
+        inputs = new TextInputLayout[]{
                 binding.user,
                 binding.password,
         };
         binding.btnSubmit.setOnClickListener((view) -> {
-            for(final TextInputLayout item: inputs){
+            for (final TextInputLayout item : inputs) {
                 final String text = String.valueOf(Objects.requireNonNull(item.getEditText()).getText()).trim();
-                if (text.isEmpty()){
+                if (text.isEmpty()) {
                     item.setError("Campo não pode ser vazio");
                 }
                 item.getEditText().setOnFocusChangeListener((a, b) -> {
@@ -43,7 +56,19 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    public static void navigate(Context context){
+    private void recoveryState(Bundle savedInstanceState) {
+        for (final TextInputLayout item : inputs) {
+            final String key = String.valueOf(item.getId());
+
+            final String recovery = savedInstanceState.getString(key);
+            Log.i(TAG, "Recovering State " + recovery);
+            Objects.requireNonNull(item.getEditText()).setText(recovery);
+        }
+    }
+
+    private static final String TAG = "LOG_IN_ACTIVITY";
+
+    public static void navigate(Context context) {
         final Intent intent = new Intent(context, LoginActivity.class);
         context.startActivity(intent);
     }
